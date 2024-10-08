45a22be3046f37c66e83e9c4d4bb03f9792f03fa
Urtzi Odriozola <uodriozola@codesyntax.com>
2024-10-09 00:44:14 +0200

    feat: Make po2dataset executable from command line + added an argument to add open licence file automatically

--- po2dataset/po2dataset.py
+++ po2dataset/po2dataset.py
@@ -1,9 +1,24 @@
+#!/usr/bin/env python3
+
 import os
 import sys
 import polib
 import argparse
 import json
+import zipfile
 import shutil
+import requests
+
+DEFAULT_FORMAT = "argosdata"
+FORMAT_CHOICES = [DEFAULT_FORMAT, "zip"]
+
+DEFAULT_LICENSE = "CC0"
+LICENSE_MAPPING = {
+    DEFAULT_LICENSE: "https://creativecommons.org/publicdomain/zero/1.0/legalcode.txt",
+    "CC-BY": "https://creativecommons.org/licenses/by/4.0/legalcode.txt",
+    "CC-BY-SA": "https://creativecommons.org/licenses/by-sa/4.0/legalcode.txt",
+}
+LICENSE_CHOICES = [key for key, value in LICENSE_MAPPING.items()]
 
 
 def create_workdir(output, name, source_code, target_code):
@@ -15,14 +30,18 @@ def create_workdir(output, name, source_code, target_code):
         print("Working directory '%s' created successfully" % path)
         return path
     except OSError as error:
-        print("Working directory '%s' can not be created" % path)
+        print(
+            "Working directory '%s' can not be created. This folder already exists!"
+            % path
+        )
         sys.exit()
 
 
 def create_dataset(path, output):
     pofile = polib.pofile(path)
     total_strings = len(pofile)
-    print("{} strings to process".format(total_strings))
+    strings = len(pofile.translated_entries())
+    print("{} from {} strings to process".format(strings, total_strings))
 
     f_source = open("{}/source".format(output), "w")
     f_target = open("{}/target".format(output), "w")
@@ -30,12 +49,12 @@ def create_dataset(path, output):
         print(entry.msgid, file=f_source)
         print(entry.msgstr, file=f_target)
 
-        process = (i / total_strings) * 100
+        process = (i / strings) * 100
         print("%{:.0f} completed...".format(process), end="\r")
     f_source.close()
     f_target.close()
     print("\n")
-    return total_strings
+    return strings
 
 
 def create_metadata(output, name, source_code, target_code, ref, total_strings):
@@ -51,9 +70,26 @@ def create_metadata(output, name, source_code, target_code, ref, total_strings):
         json.dump(metadata, f, indent=2)
 
 
-def make_zip(output, format="zip"):
-    print("Creating {} file".format(format))
-    shutil.make_archive(output, "zip", output)
+def add_license(output, license=DEFAULT_LICENSE):
+    print("Adding {} license to the package...".format(license))
+    response = requests.get(LICENSE_MAPPING[license])
+    if response.status_code == 200:
+        with open("{}/LICENSE".format(output), "w") as f:
+            f.write(response.text)
+    else:
+        print("Couldn't retrieve the license due to connection issues")
+
+
+def make_zip(output, format=DEFAULT_FORMAT):
+    print("Creating {} file in {}".format(format, output))
+    zip_path = "{}.{}".format(output, format)
+    with zipfile.ZipFile(zip_path, "w") as zipf:
+        # Direktorioan dauden fitxategi guztiak zip fitxategian sartuko ditugu
+        for root, dirs, files in os.walk(output):
+            for file in files:
+                file_path = os.path.join(root, file)
+                # Fitxategia zip fitxategira gehitu
+                zipf.write(file_path, os.path.relpath(file_path, output))
     print("Clearing working directory")
     shutil.rmtree(output)
 
@@ -77,6 +113,24 @@ def main():
     parser.add_argument(
         "-o", "--output", type=str, required=False, help="Output file path"
     )
+    parser.add_argument(
+        "-f",
+        "--format",
+        type=str,
+        choices=FORMAT_CHOICES,
+        required=False,
+        help="Extension name of the zip file",
+    )
+
+    parser.add_argument(
+        "-l",
+        "--license",
+        type=str,
+        choices=LICENSE_CHOICES,
+        required=False,
+        help="License file to be included in the package (select choice or provide custom file path)",
+    )
+
     args = parser.parse_args()
 
     output = args.output or ""
@@ -90,7 +144,12 @@ def main():
     create_metadata(
         output, args.name, args.source_code, args.target_code, args.ref, total_strings
     )
-    make_zip(output)
+
+    license = args.license or DEFAULT_LICENSE
+    add_license(output, license)
+
+    format = args.format or DEFAULT_FORMAT
+    make_zip(output, format)
 
     print("Dataset created successfully!!")
 
